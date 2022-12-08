42b1f8e3f902164a9c358f91cdb71fc9ff739a06
luciano <rodriguezluciano761@gmail.com>
2022-12-08 17:25:48 -0300

    clases primer ejercicio

---
+++ clases.js
@@ -0,0 +1,23 @@
+class ProductManager{
+    constructor(){
+        this.products=[
+            this.title = title,
+            this.description = description,
+            this.precio = precio,
+            this.thumbail = thumbail,
+            this.code = code,
+            this.stock = stock
+        ]
+    }
+    addProduct(){
+        if(){
+            
+        }  
+    }
+    getProducts(){
+
+    }
+    getProducById(){
+
+    }
+}
