d6dc3ddb8e0aeadac6cbec69856017618c0065c1
luciano <rodriguezluciano761@gmail.com>
2022-12-08 18:32:19 -0300

    primera entrega

--- clases.js
+++ clases.js
@@ -1,23 +1,27 @@
 class ProductManager{
-    constructor(){
-        this.products=[
-            this.title = title,
-            this.description = description,
-            this.precio = precio,
-            this.thumbail = thumbail,
-            this.code = code,
-            this.stock = stock
-        ]
+    static id = 0;
+    static products;
+    constructor(title, description,precio,thumbail,code,stock){
+        ProductManager.id +=1
+        this.products=[]
+        this.title = title,
+        this.description = description,
+        this.precio = precio,
+        this.thumbail = thumbail,
+        this.code = code,
+        this.stock = stock
     }
     addProduct(){
-        if(){
-            
-        }  
+        this.products.push([{title:this.title,description:this.description, precio: this.precio, stock:this.stock}])
     }
     getProducts(){
-
+        console.log("getProducts", this.products);
     }
     getProducById(){
 
     }
 }
+
+let producto1 = new ProductManager("Fideos","carbohidratos para la alimentacion del deportista", 150, 20)
+
+producto1.addProduct()
