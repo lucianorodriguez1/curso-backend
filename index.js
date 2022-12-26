3fd492a7f6018d13050b7eac507aeb98d55ae2a0
luciano <rodriguezluciano761@gmail.com>
2022-12-26 15:57:26 -0300

    get products segundo desafio

--- index.js
+++ index.js
@@ -1,51 +1,34 @@
+const fs = require("fs");
+
+
+
 class ProductManager{
-    constructor(){
-        this.products = []
-        this.product = Product
+    constructor(path){
+        this.path = path
     }
 
-    addProduct(product){
-        if(this.getProducts().find((p)=> p.code == product.code)) return console.log(`Product with code ${product.code} already exist!`)
-        else product.id = this.getProducts().length + 1; this.products.push(product)
-    }
-    getProducts(){
-        return this.products;
+    async #readFile(){
+        try {
+            const content = await fs.promises.readFile(this.path, "utf-8")
+            const parseContent = JSON.parse(content)
+            return parseContent
+        } catch (error) {
+            console.log(error);
+        }
     }
 
-    getProductsbyId(id){
-        let producto = this.getProducts().find(p=>id == p.id)
-        if(producto !== undefined) return producto
-        throw "El producto con ID " + id + "no existe entre los disponibles"
+    async getProducts(){
+        const fileContent = await this.#readFile();
+        try {
+            if(fileContent.length === 0) throw new Error("Not products found!")
+            else console.log(fileContent);
+        } catch(error){
+            console.log("Not products found!");
+        }
+
     }
+
 }
 
-
-
-class Product extends ProductManager{
-    constructor(title, description, price, thumbnail, code, stock){
-        super();
-        this.title = title;
-        this.description = description;
-        this.price = price;
-        this.thumbnail = thumbnail;
-        this.code = code;
-        this.stock = stock;
-    }
-}
-
-const newProduct = new ProductManager();
-
-const producto1 = {
-    title: "Fideos",    
-    description: "Alimentos especialmente para deportistas de alto rendimiento",
-    price:200,
-    thumbnail: "fideos.png",
-    code: 39,
-    stock:30,
-}
-
-newProduct.addProduct(producto1)
-
-console.log(newProduct.getProducts());
-
-console.log(producto1);
+const newProduct = new ProductManager("./db.json")
+newProduct.getProducts()
