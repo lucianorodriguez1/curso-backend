99cb95311eb4974a911f50624bde62b797b93b78
luciano <rodriguezluciano761@gmail.com>
2022-12-26 21:14:24 -0300

    addProduct segundo desafio

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const [fideos] = require("./productos")
 
 
 
@@ -17,6 +18,11 @@ class ProductManager{
         }
     }
 
+    async #checkProductCode(code){
+        const fileContent = await this.#readFile();
+        return fileContent.find((obj)=>obj.code === code)
+    }
+
     async getProducts(){
         const fileContent = await this.#readFile();
         try {
@@ -28,7 +34,20 @@ class ProductManager{
 
     }
 
+    async addProduct(){
+        const fileContent = await this.#readFile()
+        if(await this.#checkProductCode(obj.code)) return console.log(`Product with code ${obj.code} is already selected`);
+    
+        try {
+            if(fileContent.length !== 0 ) await fs.promises.writeFile(this.path, JSON.stringify([...fileContent, {...obj, id: fileContent[fileContent.length - 1].id + 1}],null,2),"utf-8")
+            else await fs.promises.writeFile(this.path, JSON.stringify([{...obj, id: 1}]), "utf-8")
+        } catch (error) {
+            console.log(error);   
+        }
+    }
+
 }
 
 const newProduct = new ProductManager("./db.json")
 newProduct.getProducts()
+newProduct.addProduct(fideos)
---
+++ productos.js
@@ -0,0 +1,12 @@
+const Products =[
+    {
+        title:"fideos",
+        description:"Alimento carbohidratos",
+        price: 120,
+        thumbail:"fideos.jpg",
+        code:1,
+        stock:1350,
+    }
+]
+
+module.exports = Products
